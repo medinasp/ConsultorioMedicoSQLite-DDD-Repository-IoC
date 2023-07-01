05a3d724d1d28e2f12a479a1f0ff35d2f0117eac
medinasp <medinasp@gmail.com>
2023-06-30 21:31:06 -0300

    update Aplicacao

---
+++ ConsultorioMedico.Aplicacao/ApplicationModule.cs
@@ -0,0 +1,29 @@
+﻿using Consultorio.Infra;
+using ConsultorioMedico.Aplicacao.InterfacesServices;
+using ConsultorioMedico.Aplicacao.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsultorioMedico.Aplicacao
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services
+                .AddApplicationServices();
+
+            return services;
+        }
+
+        private static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICadMedicosService, CadMedicosService>();
+            services.AddScoped<ICadPacientesService, CadPacientesService>();
+            services.AddScoped<IProntuariosService, ProntuariosService>();
+
+            services.AddInfrastructure();
+
+            return services;
+        }
+    }
+}
