3f9314ef66a3a637aa0eb09a05820dc43a580f17
medinasp <medinasp@gmail.com>
2023-07-04 12:02:53 -0300

    update Aplicacao

--- ConsultorioMedico.Aplicacao/Services/CadMedicosService.cs
+++ ConsultorioMedico.Aplicacao/Services/CadMedicosService.cs
@@ -1,15 +1,13 @@
 ﻿using ConsultorioMedico.Aplicacao.InputModels;
 using ConsultorioMedico.Aplicacao.InterfacesServices;
 using ConsultorioMedico.Aplicacao.ViewModels;
-using ConsultorioMedico.Dominio.Entities;
-using ConsultorioMedico.Infra.Repositories;
+using ConsultorioMedico.Infra.InterfacesRepositories;
 
 namespace ConsultorioMedico.Aplicacao.Services
 {
     public class CadMedicosService : ICadMedicosService
     {
         private readonly ICadMedicosRepository _repository;
-        //private static readonly List<CadMedico> _cadMedicosList = new List<CadMedico>();
 
         public CadMedicosService(ICadMedicosRepository repository)
         {
@@ -33,21 +31,6 @@ namespace ConsultorioMedico.Aplicacao.Services
             return viewModel;
         }
 
-        //public async Task<CadMedicosViewModel> Add(CadMedicosInputModel model)
-        //{
-        //    var cadMedicos = model.ToEntity();
-        //    _cadMedicosList.Add(cadMedicos);
-
-        //    var viewModel = new CadMedicosViewModel
-        //    {
-        //        Id = cadMedicos.Id,
-        //        Nome = cadMedicos.Nome,
-        //        CPF = cadMedicos.CPF,
-        //        Especialidade = cadMedicos.Especialidade
-        //    };
-        //    return await Task.FromResult(viewModel);
-        //}
-
         public async Task<CadMedicosViewModel> GetByCode(string code)
         {
             if (!Guid.TryParse(code, out var guid))
@@ -67,26 +50,6 @@ namespace ConsultorioMedico.Aplicacao.Services
 
             return cadMedicosViewModel;
         }
-
-        //public async Task<CadMedicosViewModel> GetByCode(string code)
-        //{
-        //    if (!Guid.TryParse(code, out var guid))
-        //        return null;
-
-        //    var cadMedicos = _cadMedicosList.FirstOrDefault(m => m.Id == guid);
-        //    if (cadMedicos == null)
-        //        return null;
-
-        //    var cadMedicosViewModel = new CadMedicosViewModel
-        //    {
-        //        Id = cadMedicos.Id,
-        //        Nome = cadMedicos.Nome,
-        //        CPF = cadMedicos.CPF,
-        //        Especialidade = cadMedicos.Especialidade,
-        //    };
-
-        //    return await Task.FromResult(cadMedicosViewModel);
-        //}
 
         public async Task<List<CadMedicosViewModel>> GetByName(string name)
         {
@@ -121,19 +84,6 @@ namespace ConsultorioMedico.Aplicacao.Services
             return viewModels;
         }
 
-        //public async Task<IEnumerable<CadMedicosViewModel>> GetAll()
-        //{
-        //    var viewModels = _cadMedicosList.Select(m => new CadMedicosViewModel
-        //    {
-        //        Id = m.Id,
-        //        Nome = m.Nome,
-        //        CPF = m.CPF,
-        //        Especialidade = m.Especialidade,
-        //    });
-
-        //    return await Task.FromResult(viewModels);
-        //}
-
         public async Task<bool> Update(string id, CadMedicosInputModel model)
         {
             if (!Guid.TryParse(id, out var guid))
@@ -151,18 +101,6 @@ namespace ConsultorioMedico.Aplicacao.Services
             return true;
         }
 
-        //public async Task<bool> Update(string id, CadMedicosInputModel model)
-        //{
-        //    var cadMedicos = _cadMedicosList.FirstOrDefault(m => m.Id.ToString() == id);
-
-        //    if (cadMedicos == null)
-        //        return false;
-
-        //    cadMedicos.Update(model.Nome, model.CPF, model.Especialidade);
-
-        //    return true;
-        //}
-
         public async Task<bool> SoftDelete(string id)
         {
             if (!Guid.TryParse(id, out var guid))
@@ -177,20 +115,6 @@ namespace ConsultorioMedico.Aplicacao.Services
 
             return true;
         }
-
-        //public async Task<bool> SoftDelete(string id)
-        //{
-        //    if (!Guid.TryParse(id, out var guid))
-        //        return false;
-
-        //    var cadMedicos = _cadMedicosList.FirstOrDefault(m => m.Id == guid && m.Ativo);
-        //    if (cadMedicos == null)
-        //        return false;
-
-        //    cadMedicos.Excluir();
-
-        //    return true;
-        //}
 
         public async Task<IEnumerable<CadMedicosViewModel>> GetActives()
         {
@@ -207,20 +131,6 @@ namespace ConsultorioMedico.Aplicacao.Services
             return viewModels;
         }
 
-        //public async Task<IEnumerable<CadMedicosViewModel>> GetActives()
-        //{
-        //    var activeCadMedicos = _cadMedicosList.Where(m => m.Ativo);
-        //    var viewModels = activeCadMedicos.Select(m => new CadMedicosViewModel
-        //    {
-        //        Id = m.Id,
-        //        Nome = m.Nome,
-        //        CPF = m.CPF,
-        //        Especialidade = m.Especialidade
-        //    });
-
-        //    return await Task.FromResult(viewModels);
-        //}
-
         public async Task<bool> HardDelete(string id)
         {
             if (!Guid.TryParse(id, out var guid))
@@ -235,18 +145,5 @@ namespace ConsultorioMedico.Aplicacao.Services
 
             return true;
         }
-
-        //public async Task<bool> HardDelete(string id)
-        //{
-        //    var cadMedicos = _cadMedicosList.FirstOrDefault(m => m.Id.ToString() == id);
-
-        //    if (cadMedicos == null)
-        //        return false;
-
-        //    _cadMedicosList.Remove(cadMedicos);
-
-        //    return true;
-        //}
-
     }
 }
