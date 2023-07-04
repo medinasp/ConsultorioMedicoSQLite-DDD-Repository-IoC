439a895faf56f2822727704d5befd68b0f335c90
medinasp <medinasp@gmail.com>
2023-07-04 12:02:29 -0300

    update Aplicacao

--- ConsultorioMedico.Aplicacao/InterfacesServices/IProntuariosService.cs
+++ ConsultorioMedico.Aplicacao/InterfacesServices/IProntuariosService.cs
@@ -5,25 +5,13 @@ namespace ConsultorioMedico.Aplicacao.InterfacesServices
 {
     public interface IProntuariosService
     {
-
         Task<IEnumerable<ProntuariosViewModel>> GetAll();
-        Task<List<ProntuariosViewModel>> CriarProntuarioPorId(ProntuariosInputModel model);
+        Task<ProntuariosViewModel> CriarProntuarioPorId(ProntuariosInputModel model);
         Task<ProntuariosViewModel> ConsultaPorCodigo(string code);
         Task<List<ProntuariosViewModel>> ConsultarProntuarioPorNomeMedico(string nomeMedico);
         Task<List<ProntuariosViewModel>> ConsultarProntuarioPorNomePaciente(string nomePaciente);
         Task<bool> EditarProntuario(string id, ProntuariosInputModel model);
         Task<bool> RemoverProntuarioSoft(string id);
         Task<bool> RemoverProntuarioHard(string id);
-
-
-        //Task<IEnumerable<ProntuariosViewModel>> GetAll();
-        //Task<List<ProntuariosViewModel>> CriarProntuarioPorId(ProntuariosInputModel model);
-        //Task<ProntuariosViewModel> ConsultaPorCodigo(string code);
-        //Task<List<ProntuariosViewModel>> ConsultarProntuarioPorNomeMedico(string nomeMedico);
-        //Task<List<ProntuariosViewModel>> ConsultarProntuarioPorNomePaciente(string nomePaciente);
-        //Task<bool> EditarProntuario(string id, ProntuariosInputModel model);
-        //Task<bool> RemoverProntuarioSoft(string id);
-        //Task<bool> RemoverProntuarioHard(string id);
-
     }
 }
