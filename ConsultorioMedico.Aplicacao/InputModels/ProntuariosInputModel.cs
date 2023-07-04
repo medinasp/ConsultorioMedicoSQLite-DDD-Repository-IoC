18947497431671deb5f178ff10298ce1a70d7413
medinasp <medinasp@gmail.com>
2023-07-04 12:01:24 -0300

    update Aplicacao

--- ConsultorioMedico.Aplicacao/InputModels/ProntuariosInputModel.cs
+++ ConsultorioMedico.Aplicacao/InputModels/ProntuariosInputModel.cs
@@ -1,13 +1,15 @@
-﻿namespace ConsultorioMedico.Aplicacao.InputModels
+﻿using ConsultorioMedico.Dominio.Entities;
+
+namespace ConsultorioMedico.Aplicacao.InputModels
 {
     public class ProntuariosInputModel
     {
         public Guid MedicoId { get; set; }
-        //public string MedicoNome { get; set; }
-        //public string MedicoEspecialidade { get; set; }
         public Guid PacienteId { get; set; }
-        //public string PacienteNome { get; set; }
         public string TextoProntuario { get; set; }
+
+        public Prontuarios ToEntity()
+                 => new(MedicoId, PacienteId, TextoProntuario);
 
     }
 }
