196c0094546e986fcd82033fe546b281a0dd72cd
medinasp <medinasp@gmail.com>
2023-07-04 12:03:29 -0300

    update Aplicacao

--- ConsultorioMedico.Aplicacao/Services/ProntuariosService.cs
+++ ConsultorioMedico.Aplicacao/Services/ProntuariosService.cs
@@ -1,22 +1,13 @@
 ﻿using ConsultorioMedico.Aplicacao.InputModels;
 using ConsultorioMedico.Aplicacao.InterfacesServices;
 using ConsultorioMedico.Aplicacao.ViewModels;
-using ConsultorioMedico.Dominio.Entities;
-using ConsultorioMedico.Infra.Repositories;
-using System.Net.Http;
+using ConsultorioMedico.Infra.InterfacesRepositories;
 
 namespace ConsultorioMedico.Aplicacao.Services
 {
     public class ProntuariosService : IProntuariosService
     {
-        //private readonly HttpClient _httpClient;
         private readonly IProntuariosRepository _prontuariosRepository;
-
-        //public ProntuariosService(HttpClient httpClient, IProntuariosRepository prontuariosRepository)
-        //{
-        //    _httpClient = httpClient;
-        //    _prontuariosRepository = prontuariosRepository;
-        //}
 
         public ProntuariosService(IProntuariosRepository prontuariosRepository)
         {
@@ -29,6 +20,7 @@ namespace ConsultorioMedico.Aplicacao.Services
                 return null;
 
             var prontuarios = await _prontuariosRepository.GetById(guid.ToString());
+
             if (prontuarios == null)
                 return null;
 
@@ -49,6 +41,7 @@ namespace ConsultorioMedico.Aplicacao.Services
         public async Task<List<ProntuariosViewModel>> ConsultarProntuarioPorNomeMedico(string nomeMedico)
         {
             var prontuariosList = await _prontuariosRepository.GetByNomeMedico(nomeMedico);
+
             if (prontuariosList == null)
                 return null;
 
@@ -69,6 +62,7 @@ namespace ConsultorioMedico.Aplicacao.Services
         public async Task<List<ProntuariosViewModel>> ConsultarProntuarioPorNomePaciente(string nomePaciente)
         {
             var prontuariosList = await _prontuariosRepository.GetByNomePaciente(nomePaciente);
+
             if (prontuariosList == null)
                 return null;
 
@@ -86,78 +80,27 @@ namespace ConsultorioMedico.Aplicacao.Services
             return prontuariosViewModelList;
         }
 
-        public async Task<List<ProntuariosViewModel>> CriarProntuarioPorId(ProntuariosInputModel model)
+        public async Task<ProntuariosViewModel> CriarProntuarioPorId(ProntuariosInputModel model)
         {
-            var medicoResponse = model.MedicoId;
-            var pacienteResponse = model.PacienteId;
-
-            var prontuario = new Prontuarios(model.MedicoId,model.PacienteId, model.TextoProntuario);
-
-            //await _prontuariosRepository.Add(prontuario);
-
+            var prontuario = model.ToEntity();
             var cadProntList = await _prontuariosRepository.Add(prontuario);
+
             if (cadProntList == null)
                 return null;
 
-            var viewModel = cadProntList.Select(c => new ProntuariosViewModel
+            var viewModel = new ProntuariosViewModel
             {
-                Id = c.Id,
-                IdPaciente = c.PacienteId,
-                NomePaciente = c.PacienteNome,
-                IdMedico = c.MedicoId,
-                NomeMedico = c.MedicoNome,
-                MedicoEspecialidade = c.MedicoEspecialidade,
-                TextoProntuario = c.TextoProntuario
-            }).ToList();
+                Id = prontuario.Id,
+                IdPaciente = prontuario.PacienteId,
+                NomePaciente = prontuario.PacienteNome,
+                IdMedico = prontuario.MedicoId,
+                NomeMedico = prontuario.MedicoNome,
+                MedicoEspecialidade = prontuario.MedicoEspecialidade,
+                TextoProntuario = prontuario.TextoProntuario
+            };
 
             return viewModel;
         }
-
-
-
-        //public async Task<List<ProntuariosViewModel>> CriarProntuarioPorId(ProntuariosInputModel model)
-        //{
-        //    // Obter dados do médico da API externa
-        //    var medicoResponse = await _httpClient.GetAsync($"https://localhost:7176/api/CadMedicos/{model.MedicoId}");
-        //    if (!medicoResponse.IsSuccessStatusCode)
-        //    {
-        //        throw new Exception("Erro ao obter os dados do médico");
-        //    }
-
-        //    var medicoContent = await medicoResponse.Content.ReadAsStringAsync();
-        //    var deserealizadoMedico = JsonConvert.DeserializeObject<ExternalApiInputModel>(medicoContent);
-
-        //    // Obter dados do paciente da API externa
-        //    var pacienteResponse = await _httpClient.GetAsync($"https://localhost:7099/api/CadClientes/{model.PacienteId}");
-        //    if (!pacienteResponse.IsSuccessStatusCode)
-        //    {
-        //        throw new Exception("Erro ao obter os dados do paciente");
-        //    }
-
-        //    var pacienteContent = await pacienteResponse.Content.ReadAsStringAsync();
-        //    var deserealizadoPaciente = JsonConvert.DeserializeObject<ExternalApiInputModel>(pacienteContent);
-
-        //    var prontuario = new Prontuario(deserealizadoMedico.Id, deserealizadoMedico.nome, deserealizadoMedico.especialidade, deserealizadoPaciente.Id, deserealizadoPaciente.nome, model.TextoProntuario);
-
-        //    // Persistir o prontuário no banco de dados
-        //    var cadProntList = await _prontuariosRepository.Add(prontuario);
-        //    if (cadProntList == null)
-        //        return null;
-
-        //    var viewModel = cadProntList.Select(c => new ProntuariosViewModel
-        //    {
-        //        Id = c.Id,
-        //        IdPaciente = Guid.Parse(c.PacienteId),
-        //        NomePaciente = c.PacienteNome,
-        //        IdMedico = Guid.Parse(c.MedicoId),
-        //        NomeMedico = c.MedicoNome,
-        //        EspecialidadeMedico = c.MedicoEspecialidade,
-        //        TextoProntuario = c.TextoProntuario
-        //    }).ToList();
-
-        //    return viewModel;
-
-        //}
 
         public async Task<bool> EditarProntuario(string id, ProntuariosInputModel model)
         {
@@ -222,317 +165,4 @@ namespace ConsultorioMedico.Aplicacao.Services
             return true;
         }
     }
-
-
-
-
-
-
-    //public class ProntuariosService : IProntuariosService
-    //{
-    //    private readonly ICadPacientesService _cadPacienteService;
-    //    private readonly ICadMedicosService _cadMedicoService;
-    //    private readonly IProntuariosRepository _prontuariosRepository;
-    //    private static readonly List<Prontuarios> _prontuariosList = new List<Prontuarios>();
-
-    //    public ProntuariosService(ICadPacientesService cadPacientesService, ICadMedicosService cadMedicosService)
-    //    {
-    //        _cadPacienteService = cadPacientesService;
-    //        _cadMedicoService = cadMedicosService;
-    //    }
-
-    //    //public async Task<List<ProntuariosViewModel>> CriarProntuarioPorId(ProntuariosInputModel model)
-    //    //{
-    //    //    Obter dados do médico da API externa
-    //    //   var medicoResponse = await _httpClient.GetAsync($"https://localhost:7176/api/CadMedicos/{model.MedicoId}");
-    //    //    if (!medicoResponse.IsSuccessStatusCode)
-    //    //    {
-    //    //        throw new Exception("Erro ao obter os dados do médico");
-    //    //    }
-
-    //    //    var medicoContent = await medicoResponse.Content.ReadAsStringAsync();
-    //    //    var deserealizadoMedico = JsonConvert.DeserializeObject<ExternalApiInputModel>(medicoContent);
-
-    //    //    Obter dados do paciente da API externa
-    //    //   var pacienteResponse = await _httpClient.GetAsync($"https://localhost:7099/api/CadClientes/{model.PacienteId}");
-    //    //    if (!pacienteResponse.IsSuccessStatusCode)
-    //    //    {
-    //    //        throw new Exception("Erro ao obter os dados do paciente");
-    //    //    }
-
-    //    //    var pacienteContent = await pacienteResponse.Content.ReadAsStringAsync();
-    //    //    var deserealizadoPaciente = JsonConvert.DeserializeObject<ExternalApiInputModel>(pacienteContent);
-
-    //    //    var prontuario = new Prontuario(deserealizadoMedico.Id, deserealizadoMedico.nome, deserealizadoMedico.especialidade, deserealizadoPaciente.Id, deserealizadoPaciente.nome, model.TextoProntuario);
-
-    //    //    Persistir o prontuário no banco de dados
-    //    //   var cadProntList = await _prontuariosRepository.Add(prontuario);
-    //    //    if (cadProntList == null)
-    //    //        return null;
-
-    //    //    var viewModel = cadProntList.Select(c => new ProntuariosViewModel
-    //    //    {
-    //    //        Id = c.Id,
-    //    //        IdPaciente = Guid.Parse(c.PacienteId),
-    //    //        NomePaciente = c.PacienteNome,
-    //    //        IdMedico = Guid.Parse(c.MedicoId),
-    //    //        NomeMedico = c.MedicoNome,
-    //    //        EspecialidadeMedico = c.MedicoEspecialidade,
-    //    //        TextoProntuario = c.TextoProntuario
-    //    //    }).ToList();
-
-    //    //    return viewModel;
-
-    //    //}
-
-    //    public async Task<CadMedicosViewModel> Add(CadMedicosInputModel model)
-    //    {
-    //        var cadMedico = model.ToEntity();
-    //        await _repository.Add(cadMedico);
-
-    //        var viewModel = new CadMedicosViewModel
-    //        {
-    //            Id = cadMedico.Id,
-    //            Nome = cadMedico.Nome,
-    //            CPF = cadMedico.CPF,
-    //            Especialidade = cadMedico.Especialidade,
-    //            DataCriacao = cadMedico.DataCriacao,
-    //            Ativo = cadMedico.Ativo
-    //        };
-    //        return viewModel;
-    //    }
-
-    //    public async Task<List<ProntuariosViewModel>> CriarProntuarioPorId(ProntuariosInputModel model)
-    //    {
-    //        var prontuarios = model.ToEntity();
-    //        await _prontuariosRepository.Add(prontuarios);
-
-    //        var cadProntList = await _prontuariosRepository.Add(prontuarios);
-    //        if (cadProntList == null)
-    //            return null;
-
-    //        var viewModel = cadProntList.Select(c => new ProntuariosViewModel
-    //        {
-    //            Id = c.Id,
-    //            IdPaciente = c.PacienteId,
-    //            NomePaciente = c.PacienteNome,
-    //            IdMedico = c.MedicoId,
-    //            NomeMedico = c.MedicoNome,
-    //            EspecialidadeMedico = c.MedicoEspecialidade,
-    //            TextoProntuario = c.TextoProntuario
-    //        }).ToList();
-
-    //        return viewModel;
-    //    }
-
-    //    public async Task<ProntuariosViewModel> CriarProntuarioPorId(ProntuariosInputModel model)
-    //    {
-    //        var prontuarios = model.ToEntity();
-    //        _prontuariosList.Add(prontuarios);
-
-    //        var pacientes = await _cadPacienteService.GetAll();
-    //        var medicos = await _cadMedicoService.GetAll();
-
-    //        var pacienteViewModel = pacientes.FirstOrDefault(p => p.Id == model.Paciente.Id);
-    //        var medicoViewModel = medicos.FirstOrDefault(m => m.Id == model.Medico.Id);
-
-    //        if (pacienteViewModel == null || medicoViewModel == null)
-    //        {
-    //            return null; // Tratar o caso em que o paciente ou médico não foi encontrado
-    //        }
-
-    //        var paciente = new CadPacientes(pacienteViewModel.Nome, pacienteViewModel.CPF);
-    //        var medico = new CadMedicos(medicoViewModel.Nome, medicoViewModel.CPF, medicoViewModel.Especialidade);
-
-    //        var prontuario = new Prontuarios(medico, paciente, model.TextoProntuario);
-
-    //        var cadProntList = await _prontuariosRepository.Add(prontuario);
-    //        if (cadProntList == null)
-    //            return null;
-
-    //        var viewModel = cadProntList.Select(c => new ProntuariosViewModel
-    //        {
-    //            Id = c.Id,
-    //            IdPaciente = c.PacienteId,
-    //            NomePaciente = c.PacienteNome,
-    //            IdMedico = c.MedicoId,
-    //            NomeMedico = c.MedicoNome,
-    //            EspecialidadeMedico = c.MedicoEspecialidade,
-    //            TextoProntuario = c.TextoProntuario
-    //        }).ToList();
-
-    //        return viewModel;
-    //    }
-
-    //    public async Task<ProntuariosViewModel> CriarProntuarioPorNome(ProntuariosInputModel model)
-    //    {
-    //        var prontuarios = model.ToEntity();
-    //        _prontuariosList.Add(prontuarios);
-
-    //        var pacientes = await _cadPacienteService.GetAll();
-    //        var medicos = await _cadMedicoService.GetAll();
-
-    //        var pacienteViewModel = pacientes.FirstOrDefault(p => p.Nome == model.Paciente.Nome);
-    //        var medicoViewModel = medicos.FirstOrDefault(m => m.Nome == model.Medico.Nome);
-
-    //        if (pacienteViewModel == null || medicoViewModel == null)
-    //        {
-    //            return null; // Tratar o caso em que o paciente ou médico não foi encontrado
-    //        }
-
-    //        var paciente = new CadPacientes(pacienteViewModel.Nome, pacienteViewModel.CPF);
-    //        var medico = new CadMedicos(medicoViewModel.Nome, medicoViewModel.CPF, medicoViewModel.Especialidade);
-
-    //        var prontuario = new Prontuarios(medico, paciente, model.TextoProntuario);
-
-    //        var prontuarioViewModel = new ProntuariosViewModel
-    //        {
-    //            Id = prontuarios.Id,
-    //            IdPaciente = paciente.Id,
-    //            NomePaciente = paciente.Nome,
-    //            CPFPaciente = paciente.CPF,
-    //            IdMedico = medico.Id,
-    //            NomeMedico = medico.Nome,
-    //            CPFMedico = medico.CPF,
-    //            EspecialidadeMedico = medico.Especialidade,
-    //            TextoProntuario = prontuario.TextoProntuario
-    //        };
-
-    //        return prontuarioViewModel;
-    //    }
-
-    //    public async Task<ProntuariosViewModel> ConsultarProntuarioPorNomeMedico(string nomeMedico)
-    //    {
-    //        var prontuarios = _prontuariosList.FirstOrDefault(p => p.Medico.Nome == nomeMedico);
-
-    //        if (prontuarios == null)
-    //        {
-    //            return null;
-    //        }
-
-    //        var prontuariosViewModel = new ProntuariosViewModel
-    //        {
-    //            Id = prontuarios.Id,
-    //            IdMedico = prontuarios.Medico.Id,
-    //            NomeMedico = prontuarios.Medico.Nome,
-    //            EspecialidadeMedico = prontuarios.Medico.Especialidade,
-    //            IdPaciente = prontuarios.Paciente.Id,
-    //            NomePaciente = prontuarios.Paciente.Nome,
-    //            TextoProntuario = prontuarios.TextoProntuario
-    //        };
-
-    //        return await Task.FromResult(prontuariosViewModel);
-    //    }
-
-    //    public async Task<ProntuariosViewModel> ConsultarProntuarioPorNomePaciente(string nomePaciente)
-    //    {
-    //        var prontuarios = _prontuariosList.FirstOrDefault(p => p.Paciente.Nome == nomePaciente);
-
-    //        if (prontuarios == null)
-    //        {
-    //            return null;
-    //        }
-
-    //        var prontuariosViewModel = new ProntuariosViewModel
-    //        {
-    //            Id = prontuarios.Id,
-    //            IdMedico = prontuarios.Medico.Id,
-    //            NomeMedico = prontuarios.Medico.Nome,
-    //            EspecialidadeMedico = prontuarios.Medico.Especialidade,
-    //            IdPaciente = prontuarios.Paciente.Id,
-    //            NomePaciente = prontuarios.Paciente.Nome,
-    //            TextoProntuario = prontuarios.TextoProntuario
-    //        };
-
-    //        return await Task.FromResult(prontuariosViewModel);
-    //    }
-
-    //    public async Task<ProntuariosViewModel> ConsultarProntuarioPorNomeMedicoAtivos(string nomeMedico)
-    //    {
-    //        var prontuarios = _prontuariosList.FirstOrDefault(p => p.Medico.Nome == nomeMedico && p.Ativo == true);
-
-    //        if (prontuarios == null)
-    //        {
-    //            return null;
-    //        }
-
-    //        var prontuariosViewModel = new ProntuariosViewModel
-    //        {
-    //            Id = prontuarios.Id,
-    //            IdMedico = prontuarios.Medico.Id,
-    //            NomeMedico = prontuarios.Medico.Nome,
-    //            EspecialidadeMedico = prontuarios.Medico.Especialidade,
-    //            IdPaciente = prontuarios.Paciente.Id,
-    //            NomePaciente = prontuarios.Paciente.Nome,
-    //            TextoProntuario = prontuarios.TextoProntuario
-    //        };
-
-    //        return await Task.FromResult(prontuariosViewModel);
-    //    }
-
-    //    public async Task<ProntuariosViewModel> ConsultarProntuarioPorNomePacienteAtivos(string nomePaciente)
-    //    {
-    //        var prontuarios = _prontuariosList.FirstOrDefault(p => p.Paciente.Nome == nomePaciente && p.Ativo == true);
-
-    //        if (prontuarios == null)
-    //        {
-    //            return null;
-    //        }
-
-    //        var prontuariosViewModel = new ProntuariosViewModel
-    //        {
-    //            Id = prontuarios.Id,
-    //            IdMedico = prontuarios.Medico.Id,
-    //            NomeMedico = prontuarios.Medico.Nome,
-    //            EspecialidadeMedico = prontuarios.Medico.Especialidade,
-    //            IdPaciente = prontuarios.Paciente.Id,
-    //            NomePaciente = prontuarios.Paciente.Nome,
-    //            TextoProntuario = prontuarios.TextoProntuario
-    //        };
-
-    //        return await Task.FromResult(prontuariosViewModel);
-    //    }
-
-    //    public async Task<bool> EditarProntuario(string id, ProntuariosInputModel model)
-    //    {
-    //        var prontuarios = _prontuariosList.FirstOrDefault(p => p.Id.ToString() == id);
-
-    //        if (prontuarios == null)
-    //        {
-    //            return false;
-    //        }
-
-    //        prontuarios.Update(model.TextoProntuario);
-
-    //        return true;
-    //    }
-
-    //    public async Task<bool> RemoverProntuarioSoft(string id)
-    //    {
-    //        if(!Guid.TryParse(id, out var guid))
-    //            return false;
-
-    //        var prontuarios = _prontuariosList.FirstOrDefault(p => p.Id == guid && p.Ativo);
-
-    //        if(prontuarios == null)
-    //            return false;
-
-    //        prontuarios.Excluir();
-
-    //        return true;
-    //    }
-
-    //    public async Task<bool> RemoverProntuarioHard(string id)
-    //    {
-    //        var prontuarios = _prontuariosList.FirstOrDefault(p => p.Id.ToString() == id);
-
-    //        if (prontuarios == null)
-    //            return false;
-
-    //        _prontuariosList.Remove(prontuarios);
-
-    //        return true;
-    //    }
-
-    //}
 }
