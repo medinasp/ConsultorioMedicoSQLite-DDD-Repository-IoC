3bb41ec8f2407e675e6c0a5414f37b19e9fc2af0
medinasp <medinasp@gmail.com>
2023-07-08 16:08:40 -0300

    update Interface

---
+++ ConsultorioMedico.Infra/InterfacesRepositories/ICadMedicosRepository.cs
@@ -0,0 +1,16 @@
+﻿using ConsultorioMedico.Dominio.Entities;
+
+namespace ConsultorioMedico.Infra.InterfacesRepositories
+{
+    public interface ICadMedicosRepository
+    {
+        Task Add(CadMedicos cadMedico);
+        Task<CadMedicos> GetByCode(string code);
+        Task<List<CadMedicos>> GetByName(string name);
+        Task<IEnumerable<CadMedicos>> GetAll();
+        Task Update(CadMedicos cadMedico);
+        Task<IEnumerable<CadMedicos>> GetActives();
+        Task<bool> SoftDelete(CadMedicos cadMedico);
+        Task HardDelete(CadMedicos cadMedico);
+    }
+}
